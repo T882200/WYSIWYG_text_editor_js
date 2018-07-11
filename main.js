ffbccd966ce9e4e1fbd38c77a095ea4809ad3ac8
t8822_000 <t882200@gmail.com>
2018-07-11 21:43:11 +0300

    move the code to seperate file

---
+++ main.js
@@ -0,0 +1,31 @@
+function enableEditMode(){
+  richTextField.document.designMode = 'On';
+}
+
+function execCmd(command) {
+  richTextField.document.execCommand(command, false, null);  
+}
+
+function execCmdWithArg(command, arg) {
+  richTextField.document.execCommand(command, false, arg);  
+}
+
+function toggleSource() {
+  if(showingSourceCode){
+    richTextField.document.getElementsByTagName('body')[0].innerHTML = richTextField.document.getElementsByTagName('body')[0].textContent;
+    showingSourceCode = false;
+  }else{
+    richTextField.document.getElementsByTagName('body')[0].textContent = richTextField.document.getElementsByTagName('body')[0].innerHTML;
+    showingSourceCode = true;
+  }
+}
+
+function toggleEdit() {
+  if(isInEditMode){
+    richTextField.document.designMode = 'Off';
+    isInEditMode = false;
+  }else{
+    richTextField.document.designMode = 'On';
+    isInEditMode = true;
+  }
+}
